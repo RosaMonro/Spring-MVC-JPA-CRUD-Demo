29f202e6f990bcefd2b6a7d0a77990df81e19c0e
RosaMonro <rosamaria.monro@gmail.com>
2024-02-08 10:32:56 +0100

    añado codigo nuevo de Victor comentado. ANtes de IMG

--- src/main/java/com/example/SpringMvcJpaCrudDemoApplication.java
+++ src/main/java/com/example/SpringMvcJpaCrudDemoApplication.java
@@ -189,3 +189,124 @@ public class SpringMvcJpaCrudDemoApplication implements CommandLineRunner {
 	}
 
 }
+
+// --  Nuevo código para el main principal:
+
+// --  Podemos "cargarnos" correoService y telefonoService
+
+
+
+
+// 	// Primero : Creamos departamentos
+
+//     Departamento dpt1 = Departamento.builder()
+//     .nombre("RRHH")
+//     .build();
+
+// Departamento dpt2 = Departamento.builder()
+//     .nombre("INFORMATICA")
+//     .build();
+
+// Departamento dpt3 = Departamento.builder()
+//     .nombre("CONTABILIDAD")
+//     .build();
+
+// // departamentoService.persitirDpto(dpt1);		
+// // departamentoService.persitirDpto(dpt2);
+// // departamentoService.persitirDpto(dpt3);
+
+
+// // Empleados
+
+// Empleado emp1 = Empleado.builder()
+// .nombre("Aurora")
+// .primerApellido("Duque")
+// .fechaAlta(LocalDate.of(2000, Month.JANUARY, 12))
+// .salario(2500.50)
+// .departamento(dpt1)
+// .genero(Genero.OTRO)
+// .build();
+
+// Empleado emp2 = Empleado.builder()
+// .nombre("Rosa Aurora")
+// .primerApellido("Duque")
+// .fechaAlta(LocalDate.of(2000, Month.JANUARY, 12))
+// .salario(2540.50)
+// .departamento(dpt2)
+// .genero(Genero.MUJER)
+// .build();
+
+// // Telefonos 
+
+// List<Telefono> telefonosEmpleado1 = new ArrayList<>(); 
+
+// Telefono telefono1Empleado1 = Telefono.builder()
+// .numero("96123456")
+// .empleado(emp1)
+// .build();
+
+// Telefono telefono2Empleado1 = Telefono.builder()
+// .numero("97123456") 
+// .empleado(emp1)
+// .build();
+
+// telefonosEmpleado1.add(telefono1Empleado1);
+// telefonosEmpleado1.add(telefono2Empleado1);	
+
+// emp1.setTelefonos(telefonosEmpleado1);
+
+// List<Telefono> telefonosEmpleado2 = new ArrayList<>(); 
+
+// Telefono telefono1Empleado2 = Telefono.builder()
+// .numero("98123456")
+// .empleado(emp2)
+// .build();
+
+// Telefono telefono2Empleado2 = Telefono.builder()
+// .numero("99123456") 
+// .empleado(emp2)
+// .build();
+
+// telefonosEmpleado2.add(telefono1Empleado2);
+// telefonosEmpleado2.add(telefono2Empleado2);
+
+// emp2.setTelefonos(telefonosEmpleado2);
+
+// // Correos 
+
+// List<Correo> correosEmpleado1 = new ArrayList<>();
+
+// Correo correo1Empleado1 = Correo.builder()
+// .correo("aaaa@gmail.com")
+// .empleado(emp1)
+// .build();
+
+// Correo correo2Empleado1 = Correo.builder()
+// .correo("bbbb@gmail.com")
+// .empleado(emp1)
+// .build();
+
+// correosEmpleado1.add(correo1Empleado1);
+// correosEmpleado1.add(correo2Empleado1);
+
+// emp1.setCorreos(correosEmpleado1);
+
+// List<Correo> correosEmpleado2 = new ArrayList<>();
+
+// Correo correo1Empleado2 = Correo.builder()
+// .correo("ccccc@gmail.com")
+// .empleado(emp2)
+// .build();
+
+// Correo correo2Empleado2 = Correo.builder()
+// .correo("dddd@gmail.com")
+// .empleado(emp2)
+// .build();
+
+// correosEmpleado2.add(correo1Empleado2);
+// correosEmpleado2.add(correo2Empleado2);
+
+// emp2.setCorreos(correosEmpleado2);
+
+// empleadoService.persistirEmpleado(emp1);
+// empleadoService.persistirEmpleado(emp2);
